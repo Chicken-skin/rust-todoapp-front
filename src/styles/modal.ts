ce8620e9279cedcda4c367bdbc02e0e02148ed6e
Chicken-skin <taiga5.11a@i.softbank.jp>
2024-03-21 21:53:58 +0900

    add edit todo process

---
+++ src/styles/modal.ts
@@ -0,0 +1,12 @@
+import { bgcolor } from "@mui/system";
+
+export const modalInnerStyle = {
+  position: "absolute",
+  top: "50%",
+  lett: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
